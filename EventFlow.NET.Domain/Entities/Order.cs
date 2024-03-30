d03c1c4bc3361f6c551c62f55a6eaefb21e39cd5
ayoub chaib <61378289+Vanhayten@users.noreply.github.com>
2024-03-30 22:25:36 +0000

    Creating the Domain Project

---
+++ EventFlow.NET.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using EventFlow.NET.Domain.Common;
+
+
+namespace EventFlow.NET.Domain.Entities
+{
+    public class Order: AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
